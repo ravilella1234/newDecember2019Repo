13c29dc76657d1fdf0b96988f9d197b77d986e0f
maha <maha@gmail.com>
2019-12-27 19:15:59 +0530

    project added

---
+++ NewSeleniumDecember2019/src/com/browsers/BrowserLaunch.java
@@ -0,0 +1,28 @@
+package com.browsers;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class BrowserLaunch 
+{
+
+	public static void main(String[] args)
+	{
+		WebDriver driver;
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\DELL\\Desktop\\drive\\chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.get("https://www.amazon.in");
+		
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\DELL\\Desktop\\drive\\geckodriver.exe");
+		driver=new FirefoxDriver();
+		driver.get("https://www.bestbuy.com");
+		
+		//webdriver.ie.driver
+		//webdriver.edge.driver
+
+	}
+
+}
